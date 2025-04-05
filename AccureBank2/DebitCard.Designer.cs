3e72dfa084a22d971903783bae3ff2fd59ea85e0
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2025-04-05 22:09:59 +0000

    Add files via upload

---
+++ AccureBank2/DebitCard.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AccureBank2
+{
+    partial class DebitCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label8 = new Label();
+            label9 = new Label();
+            pictureBox5 = new PictureBox();
+            label15 = new Label();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Goldenrod;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(label15);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Location = new Point(-4, -12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(130, 610);
+            panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Script", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(26, 293);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 23);
+            label1.TabIndex = 70;
+            label1.Text = "Settings";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.transactions;
+            pictureBox1.Location = new Point(26, 319);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(70, 54);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 69;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe Script", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(26, 380);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 23);
+            label8.TabIndex = 68;
+            label8.Text = "Settings";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe Script", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(16, 119);
+            label9.Name = "label9";
+            label9.Size = new Size(102, 23);
+            label9.TabIndex = 67;
+            label9.Text = "Main Menu";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.accoint;
+            pictureBox5.Location = new Point(26, 229);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(70, 54);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 63;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe Script", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label15.Location = new Point(22, 202);
+            label15.Name = "label15";
+            label15.Size = new Size(81, 23);
+            label15.TabIndex = 66;
+            label15.Text = "Accounts";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.settings;
+            pictureBox6.Location = new Point(26, 406);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(70, 54);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 64;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.bank;
+            pictureBox7.Location = new Point(26, 145);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(70, 54);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 65;
+            pictureBox7.TabStop = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.card;
+            pictureBox4.Location = new Point(23, 24);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(80, 75);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 16;
+            pictureBox4.TabStop = false;
+            // 
+            // DebitCard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1262, 508);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DebitCard";
+            Text = "DebitCard";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label8;
+        private Label label9;
+        private PictureBox pictureBox5;
+        private Label label15;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox4;
+    }
+}
---
+++ AccureBank2/DebitCard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccureBank2
+{
+    public partial class DebitCard : Form
+    {
+        public DebitCard()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            MainMenu Obj = new MainMenu();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            AddAccounts Obj = new AddAccounts();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Transactions Obj = new Transactions();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            Settings Obj = new Settings();
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
